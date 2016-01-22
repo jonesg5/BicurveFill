dc708ef8a856f6069a6d47c897d35d93396304b5
Geoffrey Jones <jonesg5@rpi.edu>
2016-01-22 17:19:24 -0500

    Set line style and check output arguments

--- bicurveFill.m
+++ bicurveFill.m
@@ -5,7 +5,14 @@ function h = bicurveFill( x1, y1, x2, y2, colorSpec )
 x = [(x1(:))', fliplr((x2(:))')];
 y = [(y1(:))', fliplr((y2(:))')];
 
-h = fill(x, y, colorSpec);
+h0 = fill(x, y, colorSpec);
+
+set(h0, 'LineStyle', 'none');
+
+nargoutchk(0,1);
+if nargout == 1
+    h = h0;
+end
 
 end
 
